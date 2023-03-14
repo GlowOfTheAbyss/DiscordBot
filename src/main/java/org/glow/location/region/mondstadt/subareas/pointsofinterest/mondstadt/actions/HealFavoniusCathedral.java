909e9fa741b10b3972082e27e70f8813fd8625ad
Glow <vr837017@gmail.com>
2023-03-14 22:24:50 +0300

    add show info functional

--- src/main/java/org/glow/location/region/mondstadt/subareas/pointsofinterest/mondstadt/actions/HealFavoniusCathedral.java
+++ src/main/java/org/glow/location/region/mondstadt/subareas/pointsofinterest/mondstadt/actions/HealFavoniusCathedral.java
@@ -4,6 +4,7 @@ import discord4j.core.object.entity.Message;
 import discord4j.core.spec.EmbedCreateSpec;
 import org.glow.fileManager.Save;
 import org.glow.location.Action;
+import org.glow.location.region.mondstadt.subareas.pointsofinterest.mondstadt.FavoniusCathedral;
 import org.glow.person.Player;
 
 public class HealFavoniusCathedral extends Action {
@@ -17,6 +18,32 @@ public class HealFavoniusCathedral extends Action {
     @Override
     public void startAction(Message message, Player player) {
 
+        if (message.getContent().split(" ").length == 1) {
+
+            showInfo(message);
+
+        } else {
+
+            heal(message, player);
+
+        }
+
+    }
+
+    private void showInfo(Message message) {
+
+        EmbedCreateSpec.Builder builder = EmbedCreateSpec.builder();
+        builder.title(FavoniusCathedral.getFavoniusCathedral().getName());
+        builder.description("**Вы можете заплатить, что бы вас полечили:**\n" + "\n"
+                + "10 :pig2: = 10 HP");
+
+        message.getChannel().block().createMessage(builder.build()).block();
+        message.delete().block();
+
+    }
+
+    private void heal(Message message, Player player) {
+
         EmbedCreateSpec.Builder builder = EmbedCreateSpec.builder();
 
         if (message.getContent().split(" ").length != 2) {
@@ -26,17 +53,15 @@ public class HealFavoniusCathedral extends Action {
             return;
         }
 
-        int number;
+        int healCoast;
         try {
-            number = Integer.parseInt(message.getContent().split(" ")[1]);
+            healCoast = Integer.parseInt(message.getContent().split(" ")[1]);
         } catch (NumberFormatException exception) {
             builder.title("Не найдено число");
             message.getChannel().block().createMessage(builder.build()).block();
             message.delete().block();
             return;
         }
-
-        int healCoast = number;
 
         if (player.getCoins() < healCoast) {
             builder.title(player.getName() + " у вас недостаточно :pig2:");
@@ -46,13 +71,13 @@ public class HealFavoniusCathedral extends Action {
         }
 
         player.setCoins(player.getCoins() - healCoast);
-        player.setHealth(player.getHealth() + (number));
+        player.setHealth(player.getHealth() + (healCoast));
         if (player.getHealth() > player.getMaxHealth()) {
             player.setHealth(player.getMaxHealth());
         }
         Save.getSave().saveFile(player);
 
-        builder.title(player.getName() + " востановил " + number + " поинтов здоровья");
+        builder.title(player.getName() + " востановил " + healCoast + " поинтов здоровья");
         builder.description(":pig2: " + player.getCoins() + "\n"
                 + "Энергия: " + player.getEnergy() + "\n"
                 + "Здоровье: " + player.getHealth() + "\n"
