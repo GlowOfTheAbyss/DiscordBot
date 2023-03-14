f1f6b418c8c6b6cbad631c12f9aee199669ccde4
Glow <vr837017@gmail.com>
2023-03-14 21:53:35 +0300

    remove old code

--- src/main/java/org/glow/storage/Inventory.java
+++ src/main/java/org/glow/storage/Inventory.java
@@ -138,24 +138,6 @@ public class Inventory {
         this.bag = bag;
     }
 
-//    public boolean addToBag(Item items) {
-//        if (bag.size() <= 10) {
-//            bag.add(items);
-//            return true;
-//        } else {
-//            return false;
-//        }
-//    }
-//
-//    public boolean removeFromBag(Item items) {
-//        if (bag.contains(items)) {
-//            bag.remove(items);
-//            return true;
-//        } else {
-//            return false;
-//        }
-//    }
-
     public int getBagSize() {
         return bagSize;
     }
