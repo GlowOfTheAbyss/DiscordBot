4bf1684a21e4e05498dc42075c6175c6be5cf03c
Glow <vr837017@gmail.com>
2023-03-21 23:37:30 +0300

    add constructor

--- src/main/java/org/glow/actions/Battle.java
+++ src/main/java/org/glow/actions/Battle.java
@@ -1,7 +1,24 @@
 package org.glow.actions;
 
+import discord4j.core.object.entity.Message;
+import org.glow.person.Person;
+
 public class Battle {
 
+    private Person attacker;
+    private Person defender;
+    private Message message;
 
+    public Battle(Message message, Person attacker, Person defender) {
+        this.attacker = attacker;
+        this.defender = defender;
+        this.message = message;
+    }
+
+    public void start() {
+
+
+
+    }
 
 }
