11654b346ade1835671ec3c64b8665e16a669c62
Glow <vr837017@gmail.com>
2023-03-30 11:16:38 +0300

    reduced latency between messages

--- src/main/java/org/glow/actions/Battle.java
+++ src/main/java/org/glow/actions/Battle.java
@@ -42,7 +42,7 @@ public class Battle {
             }
 
             try {
-                Thread.sleep(5000);
+                Thread.sleep(3000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
