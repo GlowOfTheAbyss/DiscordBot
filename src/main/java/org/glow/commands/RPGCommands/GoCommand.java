1ce953b040c4b3c9a5c69ec2aa325b4eeb973198
Glow <vr837017@gmail.com>
2023-04-04 16:24:26 +0300

    changing the import

--- src/main/java/org/glow/commands/RPGCommands/GoCommand.java
+++ src/main/java/org/glow/commands/RPGCommands/GoCommand.java
@@ -3,8 +3,8 @@ package org.glow.commands.RPGCommands;
 import discord4j.core.object.entity.Message;
 import org.glow.commands.Command;
 import org.glow.fileManager.Save;
-import org.glow.location.region.mondstadt.subareas.pointsofinterest.starfellValley.WhisperingWoods;
-import org.glow.location.region.mondstadt.subareas.pointsofinterest.starfellValley.actions.WhisperingWoodsAdventures;
+import org.glow.location.region.mondstadt.subareas.WhisperingWoods;
+import org.glow.location.region.mondstadt.subareas.actions.WhisperingWoodsAdventures;
 import org.glow.person.Player;
 
 public class GoCommand extends Command {
