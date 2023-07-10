1526cf5bf67a045f0ccc7cc89511939cd24ac5fe
Glow <vr837017@gmail.com>
2023-07-10 18:51:26 +0300

    remove old player check, add MessageSender support, fix name bug

--- src/main/java/org/glow/commands/RPGCommands/BuyCommand.java
+++ src/main/java/org/glow/commands/RPGCommands/BuyCommand.java
@@ -1,11 +1,13 @@
 package org.glow.commands.RPGCommands;
 
 import discord4j.core.object.entity.Message;
+import org.glow.Main;
 import org.glow.commands.Command;
 import org.glow.map.regions.mondstadt.subRegions.FavoniusCathedral;
 import org.glow.map.regions.mondstadt.subRegions.SchulzsBlacksmith;
 import org.glow.map.regions.mondstadt.subRegions.actions.BuyInFavoniusCathedral;
 import org.glow.map.regions.mondstadt.subRegions.actions.BuyInSchulzsBlacksmith;
+import org.glow.message.MessageSender;
 import org.glow.person.PersonManager;
 import org.glow.person.Player;
 
@@ -15,29 +17,36 @@ public class BuyCommand extends Command {
 
     private BuyCommand() {
         setName("buy");
-        setInfo("""
-                комманда для покупки предметов в специальных местах
-                !buy - показать предметы доступные для покупки
-                !buy [название предмета] - купить предмет
-                """);
+        String info = """
+                команда для покупки предметов в специальных местах
+                %s%s - показать предметы доступные для покупки
+                %s%s [название предмета] - купить предмет
+                """;
+        setInfo(String.format(info,
+                Main.systems.commandPrefix, getName(),
+                Main.systems.commandPrefix, getName()));
     }
 
     @Override
     public void start(Message message) {
 
-        Player player = userToPlayer(message);
-        if (player == null) {
-            return;
-        }
+        try {
 
-        if (playerInBattle(player, message)) {
-            return;
-        }
+            Player player = userToPlayer(message);
 
-        if (PersonManager.getInstance().getPlayerRegion(player) instanceof SchulzsBlacksmith) {
-            new BuyInSchulzsBlacksmith(message, player).startAction();
-        } else if (PersonManager.getInstance().getPlayerRegion(player) instanceof FavoniusCathedral) {
-            new BuyInFavoniusCathedral(message, player).startAction();
+            if (playerInBattle(player)) {
+                MessageSender.getInstance().sendMessageInChannel(message, "Находясь в битве нельзя это использовать");
+                return;
+            }
+
+            if (PersonManager.getInstance().getPlayerRegion(player) instanceof SchulzsBlacksmith) {
+                new BuyInSchulzsBlacksmith(message, player).startAction();
+            } else if (PersonManager.getInstance().getPlayerRegion(player) instanceof FavoniusCathedral) {
+                new BuyInFavoniusCathedral(message, player).startAction();
+            }
+
+        } catch (IllegalArgumentException exception) {
+            MessageSender.getInstance().sendMessageInChannel(message, exception.getMessage());
         }
 
     }
