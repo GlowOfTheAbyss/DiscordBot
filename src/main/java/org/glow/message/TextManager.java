13e6520aad4ba19bccf67c985737e754de816052
Glow <vr837017@gmail.com>
2023-07-11 17:31:14 +0300

    added many methods for text output

--- src/main/java/org/glow/message/TextManager.java
+++ src/main/java/org/glow/message/TextManager.java
@@ -1,6 +1,9 @@
 package org.glow.message;
 
+import org.glow.item.Item;
+import org.glow.magic.Spell;
 import org.glow.person.Player;
+import org.glow.storage.InventoryManager;
 
 public class TextManager {
 
@@ -10,18 +13,112 @@ public class TextManager {
 
     public String getPlayerParameters(Player player) {
 
-        String standardDescription = """
+        String playerParameters = """
                 %s: %s
                 %s: %s
                 %s: %s
                 %s: %s
                 """;
 
-        return String.format(standardDescription,
+        return String.format(playerParameters,
                 Parameters.COINS.getName(), player.getCoins(),
                 Parameters.ENERGY.getName(), player.getEnergy(),
                 Parameters.HEALTH.getName(), player.getHealth(),
                 Parameters.MANA.getName(), player.getMana());
+
+    }
+
+    public String getPlayerCharacteristic(Player player) {
+
+        String playerCharacteristic = """
+                %s: %s
+                %s: %s
+                %s: %s
+                %s: %s
+                %s: %s
+                %s: %s
+                """;
+
+        return String.format(playerCharacteristic,
+                Characteristic.STRENGTH.getName(), player.getStrength(),
+                Characteristic.ENDURANCE.getName(), player.getEndurance(),
+                Characteristic.AGILITY.getName(), player.getAgility(),
+                Characteristic.INTELLIGENCE.getName(), player.getIntelligence(),
+                Characteristic.PERCEPTION.getName(), player.getPerception(),
+                Characteristic.LUCK.getName(), player.getLuck());
+    }
+
+    public String getPlayerEquippedItems(Player player) {
+
+        String playerEquippedItems = """
+                %s: %s
+                %s: %s
+                
+                %s: %s
+                %s: %s
+                %s: %s
+                %s: %s
+                %s: %s
+                
+                %s: %s
+                %s: %s
+                """;
+
+        return String.format(playerEquippedItems,
+                Equipment.DEFEND.getName(), InventoryManager.getInstance().getArmor(player.getInventory()),
+                Equipment.ATTACK.getName(), InventoryManager.getInstance().getAttack(player.getInventory()),
+
+                Equipment.HEAD.getName(), player.getInventory().getHead().getName(),
+                Equipment.BODY.getName(), player.getInventory().getBody().getName(),
+                Equipment.LEGS.getName(), player.getInventory().getLegs().getName(),
+                Equipment.RIGHT_HAND.getName(), player.getInventory().getRightHand().getName(),
+                Equipment.LEFT_HAND.getName(), player.getInventory().getLeftHand().getName(),
+
+                Equipment.NECK.getName(), player.getInventory().getNeck().getName(),
+                Equipment.FINGER.getName(), player.getInventory().getFinger().getName()
+                );
+
+    }
+
+    public String getPlayerInventory(Player player) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Сумка: ");
+
+        if (player.getInventory().getBag().isEmpty()) {
+            stringBuilder.append("Пусто");
+        } else {
+            for (Item items : player.getInventory().getBag()) {
+                stringBuilder.append(items.getName())
+                        .append(" ");
+            }
+        }
+
+        return stringBuilder.toString();
+
+    }
+
+    public String getPlayerSkillBook(Player player) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Книга заклинаний: ")
+                .append("\n");
+
+        if (player.getSpellBook().getListSpell().isEmpty()) {
+            stringBuilder.append("Пусто");
+        } else {
+            for (Spell spell : player.getSpellBook().getListSpell()) {
+
+                stringBuilder.append(spell.getSpellName())
+                        .append(" | ")
+                        .append(spell.getCoastInMana())
+                        .append(" мана")
+                        .append("\n");
+
+            }
+        }
+
+        return stringBuilder.toString();
 
     }
 
