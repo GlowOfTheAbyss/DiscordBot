ebe9451e86678b5097caf0f15b0da9c894075d09
Glow <vr837017@gmail.com>
2023-07-11 17:09:28 +0300

    actions rename to activities, transferred send methods to a new class, add MessageSender support

--- src/main/java/org/glow/commands/Command.java
+++ src/main/java/org/glow/commands/Command.java
@@ -2,14 +2,10 @@ package org.glow.commands;
 
 import discord4j.common.util.Snowflake;
 import discord4j.core.object.entity.Message;
-import discord4j.core.object.entity.channel.MessageChannel;
-import discord4j.core.spec.EmbedCreateSpec;
-import org.glow.actions.battle.Battle;
-import org.glow.actions.battle.BattleManager;
+import org.glow.activities.battle.Battle;
+import org.glow.activities.battle.BattleManager;
 import org.glow.person.PersonManager;
 import org.glow.person.Player;
-
-import java.nio.channels.Channel;
 
 public abstract class Command implements LaunchedCommand {
 
@@ -41,11 +37,18 @@ public abstract class Command implements LaunchedCommand {
                 return player;
             }
         }
-        EmbedCreateSpec.Builder builder = EmbedCreateSpec.builder();
-        builder.title("Игрок не найден");
-        message.getChannel().block().createMessage(builder.build()).block();
-        message.delete().block();
-        return null;
+
+        throw new IllegalArgumentException("Игрок не найден");
+
+    }
+
+    protected boolean playerInBattle(Player player) {
+        for (Battle battle : BattleManager.getInstance().getBattles()) {
+            if (battle.getParticipants().contains(player)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     protected boolean isNotAdmin(Player player) {
@@ -54,61 +57,6 @@ public abstract class Command implements LaunchedCommand {
 
     protected boolean lengthCheck(Message message, int length) {
         return message.getContent().split(" ").length == length;
-    }
-
-    protected void notEnoughEnergyMessage(Message message) {
-        EmbedCreateSpec.Builder builder = EmbedCreateSpec.builder();
-        builder.title("Не достаточно энергии");
-        message.getChannel().block().createMessage(builder.build()).block();
-        message.delete().block();
-    }
-
-    protected boolean playerInBattle(Player player, Message message) {
-        for (Battle battle : BattleManager.getInstance().getBattles()) {
-            if (battle.getParticipants().contains(player)) {
-
-                EmbedCreateSpec.Builder builder = EmbedCreateSpec.builder();
-                builder.title("Находясь в битве нельзя это использовать");
-                message.getChannel().block().createMessage(builder.build()).block();
-                message.delete().block();
-
-                return true;
-            }
-        }
-        return false;
-    }
-
-    protected void sendMessageInChanel(Message message, String title) {
-
-        EmbedCreateSpec.Builder builder = EmbedCreateSpec.builder();
-        builder.title(title);
-
-        message.getChannel().block().createMessage(builder.build()).block();
-        message.delete().block();
-
-    }
-
-    protected void sendMessageInChanel(Message message, String title, String description) {
-
-        EmbedCreateSpec.Builder builder = EmbedCreateSpec.builder();
-        builder.title(title);
-        builder.description(description);
-
-        message.getChannel().block().createMessage(builder.build()).block();
-        message.delete().block();
-
-    }
-
-    protected void sendMessageInChanel(Message message, String title, String description, String image) {
-
-        EmbedCreateSpec.Builder builder = EmbedCreateSpec.builder();
-        builder.title(title);
-        builder.image(image);
-        builder.description(description);
-
-        message.getChannel().block().createMessage(builder.build()).block();
-        message.delete().block();
-
     }
 
 }
